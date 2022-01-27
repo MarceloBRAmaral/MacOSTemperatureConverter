e9c346470e58bcf7d527dfabea7020476d7d0f88
MarceloBRAmaral <marceloamaral.amaral@gmail.com>
2022-01-27 16:25:54 -0500

    Initial Commit

---
+++ Temperature Converter/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Temperature Converter
+//
+//  Created by Marcelo on 2022-01-27.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
